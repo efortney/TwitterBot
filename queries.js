86240ae1d6dcfaee5fded097552843e4cd537232
devOpElliot <efortney@missouriwestern.edu>
2018-03-11 16:50:09 -0500

    added methods for populating database

---
+++ queries.js
@@ -0,0 +1,20 @@
+//=====PREPARED STATEMENTS=====//
+//Statements are created as functions in order to be used in the exports module. 
+
+module.exports = {
+    /**
+     * Inserts data into a sqlite3 database 
+     * @param{tweet} object: a tweet object
+     */
+    insertTweet : function(object) {
+
+    },
+
+    /**
+     * Inserts data into a sqlite3 database 
+     * @param{user} object: a user object
+     */
+    insertUser : function(object) {
+
+    }
+}
